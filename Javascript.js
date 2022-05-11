0d04657462418f3dd3f3b7e46ab3e51493dc247a
tomas sopranzetti <sopraaan@gmail.com>
2022-05-11 19:44:21 -0300

    verficacion numerica

--- Javascript.js
+++ Javascript.js
@@ -1,21 +1,32 @@
 
-let valor;
+let valor 
  let cadena 
 
+
+ 
+
 do {
-    valor = prompt("ingrese un numero para carcular cuantos multiples de 5 lo componen")
+    
+    valor = (prompt("ingrese un numero para carcular cuantos multiples de 5 lo componen"))
     console.log("los multiplos de 5 son")
     cadena=[]
+
+    if(isNaN(valor)){
+     
+        alert("Ingresa un numero");
+     }
+    else{
+    
     for(let i = 1 ; i <= valor; i++){
         
         if (i % 5 == 0){
             cadena.push(i)
-        
+        alert(cadena)
         }
-    }
-alert(cadena)
+    }}
+
 } while (valor != 0)
 
-asd
+
 
 
